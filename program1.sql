00da0fd35207ba5dcdf4c8dd7690ca499532a36e
ARTHI R <129277654+ArthiR02@users.noreply.github.com>
2023-07-04 09:53:34 +0530

    Add files via upload

---
+++ program1.sql
@@ -0,0 +1,22 @@
+create database  mec6;
+create table mec6.student(
+seri varchar(10),
+name varchar(20),
+roll varchar(10), 
+depart varchar(5),
+clg varchar(30),
+place varchar(10)
+);
+describe mec6.student;
+select*from mec6.student;
+insert into mec6.student values(1,"arthi","20bm006","bme","mec","namakkal"),
+(2,"karthi","20bm003","bme","mec","namakkal"),
+(3,"sabi","20bm044","bme","mec","namakkal"),
+(4,"kavi","20bm0052","bme","mec","namakkal"),
+(5,"subi","20bm67","bme","mec","namakkal"),
+(6,"devi","20bm78","bme","mec","namakkal"),
+(7,"abi","20bm005","bme","mec","namakkal"),
+(8,"vani","20bm007","bme","mec","namakkal"),
+(9,"shalu","20bm003","bme","mec","namakkal"),
+(10,"sahi","20bm009","bme","mec","namakkal"),
+(11,"ARTHI","ARTHI","ARTHI","ARTHI","ARTHI");
